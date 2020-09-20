99c89271d07312c5ac109e7c93d7eedbec37647a
AhhhHaaa <JamesHuang@users.noreply.github.com>
2020-09-20 18:23:16 +0100

    comment

--- Top Earners.sql
+++ Top Earners.sql
@@ -1,8 +1,8 @@
 --The GROUP BY statement groups rows that have the same values into summary rows, like "find the number of customers in each country".
 
 SELECT salary * months as earning,
-       count(*)
+       count(*) -- count how many employees
 FROM employee
-GROUP BY (earning) -- GROUP BY the
+GROUP BY (earning) -- GROUP BY earning to see how many employees have same earning
 ORDER BY (earning) DESC -- order from the highest to the lowest
-LIMIT 1  -- present the top 1 earner
+LIMIT 1  -- it presents the top 1 earner
